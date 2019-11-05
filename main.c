d87c0be785570138938e831f2bd3a38bc0a786cc
Harshal5 <harshalnpatil5@gmail.com>
2019-11-05 23:41:38 +0530

    new file + save

--- main.c
+++ main.c
@@ -7,6 +7,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+
+int count = 0;
+
 typedef struct node {
 	char data;
 	int wn;
@@ -30,37 +33,55 @@ void append(list *l, char c) {
 	node *new_node;
 	new_node = (node*)malloc(sizeof(node));
 	new_node->data = c;
+	new_node->wn = ++count; 
 	if((l->head) == NULL) {
-		l->rear = NULL;
+		new_node->prev = NULL;
 		l->head = new_node;
 	}
 	else {
 		new_node->prev = l->rear;
+		l->rear->next = new_node;
 	}
 	new_node->next = NULL;
 	l->rear = new_node;
-	
 }
 
 
 void print_list(list *l) {
+	
+	printw("print_list");
+	node* travel;
+	int count= 0;
+	travel = l->head;
+	while(travel != NULL) {
+		printw("%c", travel->data);
+		travel = travel->next;
+	}
+	
+}
 
+
+void save_file(list *l, char *file_name) {
+	FILE *fp;
+	fp = fopen(file_name, "w+");
 	node* travel;
 	travel = l->head;
 	while(travel != NULL) {
-		printf("%c", travel->data);
+		fputc(travel->data, fp);
 		travel = travel->next;
 	}
-
+	fclose(fp);
+	
 }
 
 int main(int argc, char *argv[]) {
-	FILE *fp;
+
 	char ch;
 	list l;
 	int i;
-	//if(argc == 2) {
-	//	fp = fopen(argv[1], "w+");
+	if(argc == 2) {
+		//fp = fopen(argv[1], "w+");
+		init_list(&l);
 		initscr();
 		raw();
 		noecho();
@@ -69,19 +90,20 @@ int main(int argc, char *argv[]) {
 		while(1) {
 			ch = getch();
 			switch(ch) {
-				case 
-			
-			
-			
-			
-			
-			if(ch == 'q')
+				case 'q':
+					break;
+				default : 
+					append(&l, ch);
+					addch(ch);	
+					break;
+			}
+			if(ch == 'q') {
+				print_list(&l);
+				save_file(&l, argv[1]);
 				break;
-			addch(ch);
-			//printw("%c ", ch);
+			}
 		}	
 		endwin();
-		
-	//}
+	}
 	return 0;
 }
