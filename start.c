aabfbdf36147fb0202b2ae5928ab1f98e58c6c5b
Harshal5 <harshalnpatil5@gmail.com>
2019-11-10 07:57:07 +0530

    final only writing

--- start.c
+++ start.c
@@ -8,8 +8,8 @@
 #include <fcntl.h>
 #include "hlist.h"
 #include "vlist.h"
-int x, y;
-
+int x, y, ROWS, COLS;
+int arr_len = 1;
 vnode *current;
 hnode *temp;
 
@@ -52,6 +52,26 @@ void save_file(vlist *vl, hlist *hl, char *file_name) {
 	
 }
 
+int highlight(char *info, char *file_name, int ROWS) {
+	int y_prev, x_prev, ch;
+	getyx(stdscr, y_prev, x_prev);
+	attron(A_REVERSE);
+	move(ROWS - 2, 0);
+	clrtoeol();
+	printw( "%s %s \n", info, file_name);
+	printw("Are you sure Y / N : ");
+	attroff(A_REVERSE);
+	printw("  ");
+	ch  = getch();
+	addch(ch);
+	refresh();
+	if(ch == 'Y')
+		return 1;
+	move(y_prev, x_prev);
+	return 0;
+}
+
+
 void print_prompt() {
 	int prev_y, prev_x;
 	getyx(stdscr, prev_y, prev_x);
@@ -75,6 +95,167 @@ void print_prompt() {
 void print_loc(int y, int x) {
 	mvprintw(0, COLS - 22, "x: %d y: %d ", x, y);
 	move(y, x);
+}
+
+void traverse_and_assign(vlist *vl, hlist *hl, int y){
+						vnode *t;
+						t = vl->top;
+						for(int i=0; i < y; i++)
+							t = t->next;
+						t->row = hl->head;
+}	
+
+char* cut_word(vlist *vl, hlist *hl, int x, char *arr){
+	hnode *start, *end ,*temp;
+	int i, flag = 0;
+	temp = hl->head;
+	for(i = 0; i < x-1; i++){
+		temp = temp->next;
+	}
+	start = temp;
+	end = temp;
+	if(start->prev != NULL){
+		while(((start->prev)->data != ' ') && ((start->prev)->data != '\n')){
+			start = start->prev;
+			if(start->prev == NULL){
+				flag = 1;
+				break;
+			}
+			else{
+				flag = 0;
+				break;
+			}
+		}
+	}
+	if(end->next != NULL){
+		while((end->next->data != ' ') && (end->next->data != '\n')){
+			end = end->next;
+			if(end->next == NULL){
+				break;
+			}
+		}
+	}
+
+	if((start->prev->data == ' ') && (end->next->data == ' ')){
+		
+	}
+
+
+	temp = start;
+	while(temp != end){
+		arr_len++;
+		temp = temp->next;
+	}
+	//printw("%d", arr_len);
+	arr = realloc(arr, arr_len);
+	temp = start;
+	i = 0;
+	arr[i] = temp->data;
+	//printw("%c", arr[i]);
+	i++;
+	do{
+		temp = temp->next; 
+		arr[i] = temp->data;
+		//printw("%c", arr[i]);
+		i++;
+	}while(temp != end);
+	
+	if(flag == 0){
+		start->prev->next = end->next;
+		end->next->prev = start->prev;
+		start->prev = NULL;
+		end->next = NULL;
+
+	hnode* now = start;
+		while( now != NULL ) {
+   			hnode* next = now->next;
+   			free(now);
+   			now = next;
+		}
+		free(now);
+	}
+	if(flag == 1){
+		current->row = end->next;
+		end->next->prev = NULL;
+		start->prev = NULL;
+		end->next = NULL;
+
+		hnode* now = start;
+		while( now != NULL ) {
+   			hnode* next = now->next;
+   			free(now);
+   			now = next;
+		}
+		free(now);
+	}
+
+	return arr;
+}
+
+char* copy_word(hlist *hl, int x, char *arr){
+	hnode *start, *end ,*temp;
+	int i;
+	temp = hl->head;
+	for(i = 0; i < x-1; i++){
+		temp = temp->next;
+	}
+	start = temp;
+	end = temp;
+	if(start->prev != NULL){
+		while(((start->prev)->data != ' ') && ((start->prev)->data != '\n')){
+			start = start->prev;
+			if(start->prev == NULL){
+				break;
+			}
+		}
+	}
+	if(end->next != NULL){
+		while((end->next->data != ' ') && (end->next->data != '\n')){
+			end = end->next;
+			if(end->next == NULL){
+				break;
+			}
+		}
+	}
+	temp = start;
+	while(temp != end){
+		arr_len++;
+		temp = temp->next;
+	}
+	//printw("%d", arr_len);
+	arr = realloc(arr, arr_len);
+	temp = start;
+	i = 0;
+	arr[i] = temp->data;
+	//printw("%c", arr[i]);
+	i++;
+	do{
+		temp = temp->next; 
+		arr[i] = temp->data;
+		//printw("%c", arr[i]);
+		i++;
+	}while(temp != end);
+	
+	return arr;
+}
+
+void paste_word(vlist *vl, hlist *hl, int *x, char *arr){
+	int i;
+	for(i = 0; i < arr_len; i++){
+		//printw("%c ", arr[i]);
+		hinsert(hl, arr[i], *x);
+		if(hlength(*hl) == 1){   //
+						vinsert(vl, hl, y);
+						//current = vl->bottom;
+						traverse_and_assign(vl, hl, y);
+						current = current->next;
+		}
+		//if(vlength(*vl) == 1)
+		//				current = vl->bottom;
+										
+
+		(*x) = (*x) + 1;
+	}
 }
 
 void print_list(vlist *vl){
@@ -113,22 +294,58 @@ void print_list(vlist *vl){
 	}while(vtravel != NULL);
 	
 }
+void quit(vlist *vl) {
+	vnode *vtravel;
+	hnode *htravel;
 
-void traverse_and_assign(vlist *vl, hlist *hl, int y){
-						vnode *t;
-						t = vl->top;
-						for(int i=0; i < y; i++)
-							t = t->next;
-						t->row = hl->head;
-}	
+	vtravel = vl->top;
+	if(vtravel == NULL) {
+		return ;	
+	}
+	if(vtravel == vl->bottom){
+		htravel = vtravel->row;
+		if(htravel == NULL) {
+			free(vtravel);
+			return ;	
+		}
 
+
+		hnode* now = htravel;
+		while( now != NULL ) {
+   			hnode* next = now->next;
+   			free(now);
+   			now = next;
+		}
+		free(vtravel);
+	}
+	while(vtravel != NULL){
+		htravel = vtravel->row;
+		hnode* now = htravel;
+			while( now != NULL ) {
+   				hnode* next = now->next;
+   				free(now);
+   				now = next;
+			}
+		vtravel = vtravel->next;	
+	}
+	vtravel = vl->top;
+	vnode* vnow = vtravel;
+	while(vnow != NULL){
+		vnode* next = vnow->next;
+   		free(vnow);
+   		vnow = next;
+	}
+}
+	
 int main(int argc, char *argv[]) {
-	char ch, prev_ch;
+	char ch, prev_ch, *arr, *c;
 	
 	hlist hl;	
 	vlist vl;
 
-	int i, flag = 0;
+	arr = (char*)malloc(10*sizeof(char));
+
+	int i, j, m, flag = 0;
 	if(argc == 2) {
 		init_hlist(&hl);
 		init_vlist(&vl);
@@ -139,6 +356,7 @@ int main(int argc, char *argv[]) {
 		keypad(stdscr, TRUE);
 		move(0, 0);
 		getyx(stdscr, y, x);
+		getmaxyx(stdscr, ROWS, COLS);
 		while(1) {
 		print_loc(y, x);
 			
@@ -203,9 +421,77 @@ int main(int argc, char *argv[]) {
 					break;
 						
 				case (char)KEY_BACKSPACE:
-					break;		
+					if((y >= 0) && (x > 0)){
+						if((x == 1)){
+							hl.head->data = ' ';
+							clear();
+							print_list(&vl);
+							move(y, x);
+							refresh();
+							}
+						else{
+							temp = hl.head;
+							for(i=0;i<(x-1);i++){
+								temp = temp->next;
+							}
+							if(x == hlength(hl)){
+								if(hlength(hl) == 1){
+									free(temp);
+								}
+								else{
+									temp->prev->next = NULL;
+									hl.rear = hl.rear->prev;
+									free(temp); 
+								}
+							}
+							else if(x < hlength(hl)){
+								if(x == 1){
+									hl.head = hl.head->next;
+									current->row = hl.head;
+									free(temp);
+								}
+								else{
+									temp->next->prev = temp->prev;
+									temp->prev->next = temp->next;
+									free(temp);
+								}
+							}
+							clear();
+							print_list(&vl);
+							move(y, --x);
+							refresh();
 
-				case 'q':
+						}	
+					}
+					break;	
+
+				case (char)KEY_F(8):
+					cut_word(&vl, &hl, x, arr);
+					clear();
+					print_list(&vl);
+					refresh();
+					break;
+
+				case (char)KEY_F(3):
+					copy_word(&hl, x, arr);
+					break;
+
+				case (char)KEY_F(4):
+					paste_word(&vl, &hl, &x, arr);
+					move(y, x);
+					clear();
+					print_list(&vl);
+					refresh();
+					break;			
+				case (char)KEY_F(5):
+					save_file(&vl, &hl, argv[1]);
+					break;
+				case (char)KEY_F(7):
+					c = "Quiting the file";
+					m = highlight(c, "", ROWS);
+					if( m == 1)
+						quit(&vl);
+					clear();
 					break;
 				case '\n':
 					if(vl.top == NULL){
@@ -295,8 +581,7 @@ int main(int argc, char *argv[]) {
 			}
 			refresh();
 			//print_prompt();	
-			if(ch == 'q') {
-				save_file(&vl, &hl, argv[1]);
+			if((ch == (char)KEY_F(7)) && (m == 1)) {
 				break;
 			}
 			prev_ch = ch;
